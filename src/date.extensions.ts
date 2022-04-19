f07815f21da4d52e978f1077feab93c495c3c1e7
Marek Ulwanski <marek@ulwanski.pl>
2022-04-19 21:57:46 +0200

    - Added automated syntax tests - Fixed syntax errors

--- src/date.extensions.ts
+++ src/date.extensions.ts
@@ -1,4 +1,4 @@
-export {}
+export {};
 
 declare global {
     interface Date {
@@ -11,97 +11,96 @@ declare global {
 }
 
 function getTimezoneOffsetString(date: Date): string {
-    const offset = date.getTimezoneOffset();
-    const hours = Math.floor(Math.abs(offset / 60)).toString().padStart(2, "0");
-    const minutes = Math.floor(Math.abs(offset % 60)).toString().padStart(2, "0");
+  const offset = date.getTimezoneOffset();
+  const hours = Math.floor(Math.abs(offset / 60)).toString().padStart(2, "0");
+  const minutes = Math.floor(Math.abs(offset % 60)).toString().padStart(2, "0");
 
-    return `${(offset <= 0 ? '+' : '-')}${hours}:${minutes}`;
+  return `${(offset <= 0 ? '+' : '-')}${hours}:${minutes}`;
 }
 
 Date.prototype.diffFrom = function(date: Date | string = new Date()): number {
-    return (this.getTime() - (new Date(date)).getTime()) / 1e3;
-}
+  return (this.getTime() - (new Date(date)).getTime()) / 1e3;
+};
 
 Date.prototype.textDiffFrom = function(date: Date): string {
-    let delta: number = this.diffFrom(date);
-    const days = Math.floor(delta / 86400);
-    delta -= days * 86400;
-    const hours = Math.floor(delta / 60 / 60);
-    delta -= hours * 60 * 60;
-    const minutes = Math.floor(delta / 60);
-    delta -= minutes * 60;
-    const seconds = delta;
+  let delta: number = this.diffFrom(date);
+  const days = Math.floor(delta / 86400);
+  delta -= days * 86400;
+  const hours = Math.floor(delta / 60 / 60);
+  delta -= hours * 60 * 60;
+  const minutes = Math.floor(delta / 60);
+  delta -= minutes * 60;
+  const seconds = delta;
 
-    const validValues = [];
+  const validValues = [];
 
-    if (days > 0) {
-        validValues.push(days > 1 ? `${days} days` : `${days} day`);
-    }
+  if (days > 0) {
+    validValues.push(days > 1 ? `${days} days` : `${days} day`);
+  }
 
-    if (hours > 0) {
-        validValues.push(hours > 1 ? `${hours} hours` : `${hours} hour`);
-    }
+  if (hours > 0) {
+    validValues.push(hours > 1 ? `${hours} hours` : `${hours} hour`);
+  }
 
-    if (minutes > 0) {
-        validValues.push(minutes > 1 ? `${minutes} minutes` : `${minutes} minute`);
-    }
+  if (minutes > 0) {
+    validValues.push(minutes > 1 ? `${minutes} minutes` : `${minutes} minute`);
+  }
 
-    if (seconds === 0 && validValues.length === 0) {
-        validValues.push('now');
-    } else if (seconds > 0 || validValues.length === 0) {
-        validValues.push(seconds > 1 ? `${seconds} seconds` : `${seconds} second`);
-    }
+  if (seconds === 0 && validValues.length === 0) {
+    validValues.push('now');
+  } else if (seconds > 0 || validValues.length === 0) {
+    validValues.push(seconds > 1 ? `${seconds} seconds` : `${seconds} second`);
+  }
 
-    return validValues.join(', ');
-}
+  return validValues.join(', ');
+};
 
 Date.prototype.addSeconds = function(seconds: number): Date {
-    this.setTime(this.getTime() + (seconds * 1e3));
+  this.setTime(this.getTime() + (seconds * 1e3));
 
-    return this;
-}
+  return this;
+};
 
 Date.prototype.toUnixTimestamp = function(): number {
-    return (this.getTime()) / 1e3;
-}
+  return (this.getTime()) / 1e3;
+};
 
 Date.prototype.format = function (format: string): string {
-    const date: Date = this;
-    return format.replace(/[A-Za-z]{1,2}/g, function (part: any) {
-        switch (part) {
-            case 'Y': return date.getFullYear().toString().slice(-2);
+  return format.replace(/[A-Za-z]{1,2}/g, (part: any) => {
+    switch (part) {
+    case 'Y': return this.getFullYear().toString().slice(-2);
 
-            case 'YY': return date.getFullYear();
+    case 'YY': return this.getFullYear();
 
-            case 'M': return (1 + date.getMonth());
+    case 'M': return (1 + this.getMonth());
 
-            case 'MM': return `0${1 + date.getMonth()}`.slice(-2);
+    case 'MM': return `0${1 + this.getMonth()}`.slice(-2);
 
-            case 'D': return date.getDate();
+    case 'D': return this.getDate();
 
-            case 'DD': return `0${date.getDate()}`.slice(-2);
+    case 'DD': return `0${this.getDate()}`.slice(-2);
 
-            case 'H': return date.getHours();
+    case 'H': return this.getHours();
 
-            case 'HH': return `0${date.getHours()}`.slice(-2);
+    case 'HH': return `0${this.getHours()}`.slice(-2);
 
-            case 'm': return date.getMinutes();
+    case 'm': return this.getMinutes();
 
-            case 'mm': return `0${date.getMinutes()}`.slice(-2);
+    case 'mm': return `0${this.getMinutes()}`.slice(-2);
 
-            case 's': return date.getSeconds();
+    case 's': return this.getSeconds();
 
-            case 'ss': return `0${date.getSeconds()}`.slice(-2);
+    case 'ss': return `0${this.getSeconds()}`.slice(-2);
 
-            case 'S': return date.getMilliseconds();
+    case 'S': return this.getMilliseconds();
 
-            case 'SS': return `00${date.getSeconds()}`.slice(-3);
+    case 'SS': return `00${this.getSeconds()}`.slice(-3);
 
-            case 'z': return date.getTimezoneOffset();
+    case 'z': return this.getTimezoneOffset();
 
-            case 'Z': return getTimezoneOffsetString(date);
+    case 'Z': return getTimezoneOffsetString(this);
 
-            default: return part.slice (part.length); // unknown code, remove
-        }
-    });
-}
+    default: return part.slice (part.length); // unknown code, remove
+    }
+  });
+};
--- test/date.test.ts
+++ test/date.test.ts
@@ -1,195 +1,195 @@
 require("../src/date.extensions");
 
 describe('diffFrom', () => {
-    test('should accept date in parameter as a string (and return 0)', () => {
-        const date = new Date("04-18-2022 14:30:00 GMT");
-        expect(date.diffFrom("04-18-2022 14:30:00 GMT")).toBe(0);
-    });
+  test('should accept date in parameter as a string (and return 0)', () => {
+    const date = new Date("04-18-2022 14:30:00 GMT");
+    expect(date.diffFrom("04-18-2022 14:30:00 GMT")).toBe(0);
+  });
 
-    test('should return zero if date in parameter is same date', () => {
-        const date = new Date("04-18-2022 14:30:00 GMT");
-        expect(date.diffFrom(date)).toBe(0);
-    });
+  test('should return zero if date in parameter is same date', () => {
+    const date = new Date("04-18-2022 14:30:00 GMT");
+    expect(date.diffFrom(date)).toBe(0);
+  });
 
-    test('should return 3600 if date in parameter is one hour earlier', () => {
-        const date = new Date("04-18-2022 14:30:00 GMT");
-        const laterDate = new Date("04-18-2022 13:30:00 GMT");
-        expect(date.diffFrom(laterDate)).toBe(3600);
-    });
+  test('should return 3600 if date in parameter is one hour earlier', () => {
+    const date = new Date("04-18-2022 14:30:00 GMT");
+    const laterDate = new Date("04-18-2022 13:30:00 GMT");
+    expect(date.diffFrom(laterDate)).toBe(3600);
+  });
 
-    test('should return -3600 if date in parameter is one timezone earlier', () => {
-        const date = new Date("04-18-2022 14:30:00 +02:00");
-        const laterDate = new Date("04-18-2022 14:30:00 +01:00");
-        expect(date.diffFrom(laterDate)).toBe(-3600);
-    });
+  test('should return -3600 if date in parameter is one timezone earlier', () => {
+    const date = new Date("04-18-2022 14:30:00 +02:00");
+    const laterDate = new Date("04-18-2022 14:30:00 +01:00");
+    expect(date.diffFrom(laterDate)).toBe(-3600);
+  });
 
-    test('should return 1 if date in parameter is one second earlier', () => {
-        const date = new Date("04-18-2022 14:30:00 +02:00");
-        const laterDate = new Date("04-18-2022 14:29:59 +02:00");
-        expect(date.diffFrom(laterDate)).toBe(1);
-    });
+  test('should return 1 if date in parameter is one second earlier', () => {
+    const date = new Date("04-18-2022 14:30:00 +02:00");
+    const laterDate = new Date("04-18-2022 14:29:59 +02:00");
+    expect(date.diffFrom(laterDate)).toBe(1);
+  });
 
-    test('should return -1 if date in parameter is one second later', () => {
-        const date = new Date("04-18-2022 14:30:00 +02:00");
-        const laterDate = new Date("04-18-2022 14:30:01 +02:00");
-        expect(date.diffFrom(laterDate)).toBe(-1);
-    });
+  test('should return -1 if date in parameter is one second later', () => {
+    const date = new Date("04-18-2022 14:30:00 +02:00");
+    const laterDate = new Date("04-18-2022 14:30:01 +02:00");
+    expect(date.diffFrom(laterDate)).toBe(-1);
+  });
 });
 
 describe('addSeconds', () => {
-    test('should add 1 second to date when param is 1', () => {
-        const date = new Date("1970-01-01T00:00:00.000Z");
-        expect(date.addSeconds(1).toISOString()).toBe("1970-01-01T00:00:01.000Z");
-    });
+  test('should add 1 second to date when param is 1', () => {
+    const date = new Date("1970-01-01T00:00:00.000Z");
+    expect(date.addSeconds(1).toISOString()).toBe("1970-01-01T00:00:01.000Z");
+  });
 
-    test('should add 1 hour to date when param is 3600', () => {
-        const date = new Date("1970-01-01T00:00:00.000Z");
-        expect(date.addSeconds(3600).toISOString()).toBe("1970-01-01T01:00:00.000Z");
-    });
+  test('should add 1 hour to date when param is 3600', () => {
+    const date = new Date("1970-01-01T00:00:00.000Z");
+    expect(date.addSeconds(3600).toISOString()).toBe("1970-01-01T01:00:00.000Z");
+  });
 
-    test('should subtract 1 hour from date when param is -3600', () => {
-        const date = new Date("1970-01-01T01:00:00.000Z");
-        expect(date.addSeconds(-3600).toISOString()).toBe("1970-01-01T00:00:00.000Z");
-    });
-})
+  test('should subtract 1 hour from date when param is -3600', () => {
+    const date = new Date("1970-01-01T01:00:00.000Z");
+    expect(date.addSeconds(-3600).toISOString()).toBe("1970-01-01T00:00:00.000Z");
+  });
+});
 
 describe('toUnixTimestamp', () => {
-    test('should return 0 for January 1st, 1970', () => {
-        const date = new Date("01-01-1970 00:00:00.000 UTC");
-        expect(date.toUnixTimestamp()).toBe(0);
-    });
+  test('should return 0 for January 1st, 1970', () => {
+    const date = new Date("01-01-1970 00:00:00.000 UTC");
+    expect(date.toUnixTimestamp()).toBe(0);
+  });
 
-    test('should return 1650292200 for Monday, 18 April 2022 12:30:00 +02:00', () => {
-        const date = new Date("2022-04-18 14:30:00 +02:00");
-        expect(date.toUnixTimestamp()).toBe(1650285000);
-    });
+  test('should return 1650292200 for Monday, 18 April 2022 12:30:00 +02:00', () => {
+    const date = new Date("2022-04-18 14:30:00 +02:00");
+    expect(date.toUnixTimestamp()).toBe(1650285000);
+  });
 });
 
 describe('format', () => {
-    test('should return formatted date for "YY-MM-DD HH:mm:ss" string', () => {
-        const date = new Date("04-18-2022 14:30:00");
-        expect(date.format("YY-MM-DD HH:mm:ss")).toBe("2022-04-18 14:30:00");
-    });
+  test('should return formatted date for "YY-MM-DD HH:mm:ss" string', () => {
+    const date = new Date("04-18-2022 14:30:00");
+    expect(date.format("YY-MM-DD HH:mm:ss")).toBe("2022-04-18 14:30:00");
+  });
 
-    test('should return formatted date for "Y-M-D H:m:s" string', () => {
-        const date = new Date("04-18-2022 04:03:00");
-        expect(date.format("Y-M-D H:m:s")).toBe("22-4-18 4:3:0");
-    });
+  test('should return formatted date for "Y-M-D H:m:s" string', () => {
+    const date = new Date("04-18-2022 04:03:00");
+    expect(date.format("Y-M-D H:m:s")).toBe("22-4-18 4:3:0");
+  });
 
-    test('should return formatted date for "YY-MM-D HH:mm:ss.S" string', () => {
-        const date = new Date("04-08-2022 14:30:00");
-        expect(date.format("YY-MM-D HH:mm:ss.S")).toBe("2022-04-8 14:30:00.0");
-    });
+  test('should return formatted date for "YY-MM-D HH:mm:ss.S" string', () => {
+    const date = new Date("04-08-2022 14:30:00");
+    expect(date.format("YY-MM-D HH:mm:ss.S")).toBe("2022-04-8 14:30:00.0");
+  });
 
-    test('should return formatted date for "YY-MM-D HH:mm:ss.SS" string', () => {
-        const date = new Date("04-08-2022 14:30:00");
-        expect(date.format("YY-MM-D HH:mm:ss.SS")).toBe("2022-04-8 14:30:00.000");
-    });
+  test('should return formatted date for "YY-MM-D HH:mm:ss.SS" string', () => {
+    const date = new Date("04-08-2022 14:30:00");
+    expect(date.format("YY-MM-D HH:mm:ss.SS")).toBe("2022-04-8 14:30:00.000");
+  });
 
-    test('should return formatted date for "YYMMDDHHmmss" string', () => {
-        const date = new Date("04-18-2022 14:30:00");
-        expect(date.format("YYMMDDHHmmss")).toBe("20220418143000");
-    });
+  test('should return formatted date for "YYMMDDHHmmss" string', () => {
+    const date = new Date("04-18-2022 14:30:00");
+    expect(date.format("YYMMDDHHmmss")).toBe("20220418143000");
+  });
 
-    test('should return formatted date for "YY-MMMM-DDD HH:mm:ss" string', () => {
-        const date = new Date("04-8-2022 14:30:00");
-        expect(date.format("YY-MMMM-DDD HH:mm:ss")).toBe("2022-0404-088 14:30:00");
-    });
+  test('should return formatted date for "YY-MMMM-DDD HH:mm:ss" string', () => {
+    const date = new Date("04-8-2022 14:30:00");
+    expect(date.format("YY-MMMM-DDD HH:mm:ss")).toBe("2022-0404-088 14:30:00");
+  });
 
-    test('should return formatted date for "YY-MM-DD HH:mm:ss z" string', () => {
-        const date = new Date("04-18-2022 14:30:00");
-        date.getTimezoneOffset = () => 360;    // Mock local timezone
-        expect(date.format("YY-MM-DD HH:mm:ss z")).toBe("2022-04-18 14:30:00 360");
-    });
+  test('should return formatted date for "YY-MM-DD HH:mm:ss z" string', () => {
+    const date = new Date("04-18-2022 14:30:00");
+    date.getTimezoneOffset = () => 360;    // Mock local timezone
+    expect(date.format("YY-MM-DD HH:mm:ss z")).toBe("2022-04-18 14:30:00 360");
+  });
 
-    test('should return formatted date for "YY-MM-DD HH:mm:ss z" string', () => {
-        const date = new Date("04-18-2022 14:30:00");
-        date.getTimezoneOffset = () => 360;    // Mock local timezone
-        expect(date.format("YY-MM-DD HH:mm:ss Z")).toBe("2022-04-18 14:30:00 -06:00");
-    });
+  test('should return formatted date for "YY-MM-DD HH:mm:ss z" string', () => {
+    const date = new Date("04-18-2022 14:30:00");
+    date.getTimezoneOffset = () => 360;    // Mock local timezone
+    expect(date.format("YY-MM-DD HH:mm:ss Z")).toBe("2022-04-18 14:30:00 -06:00");
+  });
 
-    test('should return formatted date for "YY-MM-DD HH:mm:ss z" string', () => {
-        const date = new Date("2022-04-18 14:30:00");
-        date.getTimezoneOffset = () => 360;    // Mock local timezone
-        expect(date.format("YY-MM-DD HH:mm:ss Z")).toBe("2022-04-18 14:30:00 -06:00");
-    });
+  test('should return formatted date for "YY-MM-DD HH:mm:ss z" string', () => {
+    const date = new Date("2022-04-18 14:30:00");
+    date.getTimezoneOffset = () => 360;    // Mock local timezone
+    expect(date.format("YY-MM-DD HH:mm:ss Z")).toBe("2022-04-18 14:30:00 -06:00");
+  });
 
-    test('should return formatted date for "YY-MM-DD HH:mm:ss z" string and positive timezone', () => {
-        const date = new Date("2022-04-18 14:30:00");
-        date.getTimezoneOffset = () => -600;    // Mock local timezone
-        expect(date.format("YY-MM-DD HH:mm:ss Z")).toBe("2022-04-18 14:30:00 +10:00");
-    });
+  test('should return formatted date for "YY-MM-DD HH:mm:ss z" string and positive timezone', () => {
+    const date = new Date("2022-04-18 14:30:00");
+    date.getTimezoneOffset = () => -600;    // Mock local timezone
+    expect(date.format("YY-MM-DD HH:mm:ss Z")).toBe("2022-04-18 14:30:00 +10:00");
+  });
 
-    test('should return formatted date for "YY-MM-DD HH:mm:ss z" string and negative timezone', () => {
-        const date = new Date("2022-04-18 14:30:00");
-        date.getTimezoneOffset = () => 600;    // Mock local timezone
-        expect(date.format("YY-MM-DD HH:mm:ss Z")).toBe("2022-04-18 14:30:00 -10:00");
-    });
+  test('should return formatted date for "YY-MM-DD HH:mm:ss z" string and negative timezone', () => {
+    const date = new Date("2022-04-18 14:30:00");
+    date.getTimezoneOffset = () => 600;    // Mock local timezone
+    expect(date.format("YY-MM-DD HH:mm:ss Z")).toBe("2022-04-18 14:30:00 -10:00");
+  });
 
-    test('should return formatted date for "YY-MM-DD aaHH:mm:ss z" string', () => {
-        const date = new Date("2022-04-18 14:30:00");
-        date.getTimezoneOffset = () => 240;    // Mock local timezone
-        expect(date.format("YY-MM-DD aaHH:mm:ss Z")).toBe("2022-04-18 14:30:00 -04:00");
-    });
+  test('should return formatted date for "YY-MM-DD aaHH:mm:ss z" string', () => {
+    const date = new Date("2022-04-18 14:30:00");
+    date.getTimezoneOffset = () => 240;    // Mock local timezone
+    expect(date.format("YY-MM-DD aaHH:mm:ss Z")).toBe("2022-04-18 14:30:00 -04:00");
+  });
 });
 
 describe('textDiffFrom', () => {
-    test('should return "now" when both date are same', () => {
-        const date = new Date("2022-04-18 14:30:00");
-        expect(date.textDiffFrom(date)).toBe('now');
-    });
+  test('should return "now" when both date are same', () => {
+    const date = new Date("2022-04-18 14:30:00");
+    expect(date.textDiffFrom(date)).toBe('now');
+  });
 
-    test('should return "1 second" when second date is one second in the past', () => {
-        const date = new Date("2022-04-18 14:30:01");
-        const futureDate = new Date("2022-04-18 14:30:00");
-        expect(date.textDiffFrom(futureDate)).toBe('1 second');
-    });
+  test('should return "1 second" when second date is one second in the past', () => {
+    const date = new Date("2022-04-18 14:30:01");
+    const futureDate = new Date("2022-04-18 14:30:00");
+    expect(date.textDiffFrom(futureDate)).toBe('1 second');
+  });
 
-    test('should return "10 seconds" when second date is one second in the past', () => {
-        const date = new Date("2022-04-18 14:30:10");
-        const futureDate = new Date("2022-04-18 14:30:00");
-        expect(date.textDiffFrom(futureDate)).toBe('10 seconds');
-    });
+  test('should return "10 seconds" when second date is one second in the past', () => {
+    const date = new Date("2022-04-18 14:30:10");
+    const futureDate = new Date("2022-04-18 14:30:00");
+    expect(date.textDiffFrom(futureDate)).toBe('10 seconds');
+  });
 
-    test('should return "1 minute" when second date is one minute in the past', () => {
-        const date = new Date("2022-04-18 14:31:00");
-        const futureDate = new Date("2022-04-18 14:30:00");
-        expect(date.textDiffFrom(futureDate)).toBe('1 minute');
-    });
+  test('should return "1 minute" when second date is one minute in the past', () => {
+    const date = new Date("2022-04-18 14:31:00");
+    const futureDate = new Date("2022-04-18 14:30:00");
+    expect(date.textDiffFrom(futureDate)).toBe('1 minute');
+  });
 
-    test('should return "10 minutes" when second date is ten minutes in the past', () => {
-        const date = new Date("2022-04-18 14:40:00");
-        const futureDate = new Date("2022-04-18 14:30:00");
-        expect(date.textDiffFrom(futureDate)).toBe('10 minutes');
-    });
+  test('should return "10 minutes" when second date is ten minutes in the past', () => {
+    const date = new Date("2022-04-18 14:40:00");
+    const futureDate = new Date("2022-04-18 14:30:00");
+    expect(date.textDiffFrom(futureDate)).toBe('10 minutes');
+  });
 
-    test('should return "1 hour" when second date is one hour in the past', () => {
-        const date = new Date("2022-04-18 15:30:00");
-        const futureDate = new Date("2022-04-18 14:30:00");
-        expect(date.textDiffFrom(futureDate)).toBe('1 hour');
-    });
+  test('should return "1 hour" when second date is one hour in the past', () => {
+    const date = new Date("2022-04-18 15:30:00");
+    const futureDate = new Date("2022-04-18 14:30:00");
+    expect(date.textDiffFrom(futureDate)).toBe('1 hour');
+  });
 
-    test('should return "10 hours" when second date is ten hours in the past', () => {
-        const date = new Date("2022-04-18 14:30:00");
-        const futureDate = new Date("2022-04-18 4:30:00");
-        expect(date.textDiffFrom(futureDate)).toBe('10 hours');
-    });
+  test('should return "10 hours" when second date is ten hours in the past', () => {
+    const date = new Date("2022-04-18 14:30:00");
+    const futureDate = new Date("2022-04-18 4:30:00");
+    expect(date.textDiffFrom(futureDate)).toBe('10 hours');
+  });
 
-    test('should return "1 hour, 20 minutes" when second date is one hour and 20 minutes in the past', () => {
-        const date = new Date("2022-04-18 15:50:00");
-        const futureDate = new Date("2022-04-18 14:30:00");
-        expect(date.textDiffFrom(futureDate)).toBe('1 hour, 20 minutes');
-    });
+  test('should return "1 hour, 20 minutes" when second date is one hour and 20 minutes in the past', () => {
+    const date = new Date("2022-04-18 15:50:00");
+    const futureDate = new Date("2022-04-18 14:30:00");
+    expect(date.textDiffFrom(futureDate)).toBe('1 hour, 20 minutes');
+  });
 
-    test('should return "1 day" when second date is one day in the past', () => {
-        const date = new Date("2022-04-18 14:30:00");
-        const futureDate = new Date("2022-04-17 14:30:00");
-        expect(date.textDiffFrom(futureDate)).toBe('1 day');
-    });
+  test('should return "1 day" when second date is one day in the past', () => {
+    const date = new Date("2022-04-18 14:30:00");
+    const futureDate = new Date("2022-04-17 14:30:00");
+    expect(date.textDiffFrom(futureDate)).toBe('1 day');
+  });
 
-    test('should return "10 days" when second date is ten days in the past', () => {
-        const date = new Date("2022-04-18 14:30:00");
-        const futureDate = new Date("2022-04-08 14:30:00");
-        expect(date.textDiffFrom(futureDate)).toBe('10 days');
-    });
+  test('should return "10 days" when second date is ten days in the past', () => {
+    const date = new Date("2022-04-18 14:30:00");
+    const futureDate = new Date("2022-04-08 14:30:00");
+    expect(date.textDiffFrom(futureDate)).toBe('10 days');
+  });
 });
