cf96d485ab0a462f18721a02e55717582e6fdf3f
Marek Ulwański <marek@ulwanski.pl>
2022-08-01 00:34:03 +0200

    - Fixed unit test (date in test is now in UTC timezone)

--- test/date.test.ts
+++ test/date.test.ts
@@ -176,9 +176,9 @@ describe('format', () => {
     expect(date.format("w")).toBe('31');
   });
 
-  test('should return "1659371400" for "X" string and date 1.08.2022', () => {
+  test('should return "1659378600" for "X" string and date 1.08.2022', () => {
     const date = new Date("2022-08-1 18:30:00 UTC+00:00");
-    expect(date.format("X")).toBe('1659371400');
+    expect(date.format("X")).toBe('1659378600');
   });
 });
 
