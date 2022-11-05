9032ca475bc0d4b40aed91a227d425e50c77e83b
Marek Ulwanski <marek@ulwanski.pl>
2022-11-05 18:08:52 +0100

    - Added new unit tests
    Signed-off-by: Marek Ulwański <marek@ulwanski.pl>

--- test/date.test.ts
+++ test/date.test.ts
@@ -176,6 +176,16 @@ describe('format', () => {
     expect(date.format("W")).toBe('31');
   });
 
+  test('should return "6" for "w" string and date 05.11.2022', () => {
+    const date = new Date("2022-11-05 18:30:00");
+    expect(date.format("w")).toBe('6');
+  });
+
+  test('should return "1" for "w" string and date 07.11.2022', () => {
+    const date = new Date("2022-11-07 18:30:00");
+    expect(date.format("w")).toBe('1');
+  });
+
   test('should return "1659378600" for "X" string and date "2022-08-1 18:30:00 UTC+00:00"', () => {
     const date = new Date("2022-08-1 18:30:00 UTC+00:00");
     expect(date.format("X")).toBe('1659378600');
