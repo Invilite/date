9e71a7f268c3add7c040f82f19c1090ce5031d81
Marek Ulwański <marek@ulwanski.pl>
2022-11-05 18:54:47 +0100

    Added day of week (#18)
    - Release version 1.1.0
    - %W is now week number in year instead of %w
    - Added day number in week as %w
    - Added new unit tests

--- test/date.test.ts
+++ test/date.test.ts
@@ -166,14 +166,24 @@ describe('format', () => {
     expect(date.format("B", "en-US")).toBe('April');
   });
 
-  test('should return "30" for "w" string and date 31.07.2022', () => {
+  test('should return "30" for "W" string and date 31.07.2022', () => {
     const date = new Date("2022-07-31 18:30:00");
-    expect(date.format("w")).toBe('30');
+    expect(date.format("W")).toBe('30');
   });
 
-  test('should return "31" for "w" string and date 1.08.2022', () => {
+  test('should return "31" for "W" string and date 1.08.2022', () => {
     const date = new Date("2022-08-1 18:30:00");
-    expect(date.format("w")).toBe('31');
+    expect(date.format("W")).toBe('31');
+  });
+
+  test('should return "6" for "w" string and date 05.11.2022', () => {
+    const date = new Date("2022-11-05 18:30:00");
+    expect(date.format("w")).toBe('6');
+  });
+
+  test('should return "1" for "w" string and date 07.11.2022', () => {
+    const date = new Date("2022-11-07 18:30:00");
+    expect(date.format("w")).toBe('1');
   });
 
   test('should return "1659378600" for "X" string and date "2022-08-1 18:30:00 UTC+00:00"', () => {
